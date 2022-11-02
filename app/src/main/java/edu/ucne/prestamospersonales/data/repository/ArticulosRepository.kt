98205a80d0705344f1fd9a90ce64206c7031b24a
SamDuran <samidt0186@gmail.com>
2022-11-02 06:33:07 -0400

    Modificando ArticulosRepository.kt

--- app/src/main/java/edu/ucne/prestamospersonales/data/repository/ArticulosRepository.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/data/repository/ArticulosRepository.kt
@@ -2,26 +2,49 @@ package edu.ucne.prestamospersonales.data.repository
 
 import edu.ucne.prestamospersonales.data.remote.ArticulosApi
 import edu.ucne.prestamospersonales.data.remote.dto.ArticuloDto
+import retrofit2.Response
 import javax.inject.Inject
 
 class ArticulosRepository @Inject constructor(
     private val api : ArticulosApi
 ) {
-    suspend fun save(
-        articuloId : Int, descripcion : String,
-        marca : String, existencia : Double
-    ) : ArticuloDto {
-        try{
-            return api.save(articuloId,descripcion,marca,existencia.toString())
+    //GET (todos)
+    suspend fun getArticulos() : List<ArticuloDto> {
+        try {
+            return api.getList()
         }catch (e : Exception) {
             throw e
         }
     }
-
-    suspend fun getList() : List<ArticuloDto> {
+    //POST
+    suspend fun postArticulo(articuloDto: ArticuloDto): Response<ArticuloDto> {
         try {
-            return api.getList()
-        }catch (e : Exception) {
+            return api.save(articuloDto)
+        }catch(e : Exception){
+            throw e
+        }
+    }
+    //GET (uno)
+    suspend fun getArticulo(id: Int): ArticuloDto? {
+        try {
+            return api.getArticulo(id)
+        }catch (e:Exception){
+            throw e
+        }
+    }
+    //PUT
+    suspend fun putArticulo(id:Int, newArticulo:ArticuloDto): Response<ArticuloDto> {
+        try {
+            return api.updateArticulo(id,newArticulo)
+        }catch (e :Exception) {
+            throw e
+        }
+    }
+    //DELETE
+    suspend fun deleteArticulo(id: Int): Response<ArticuloDto> {
+        try {
+            return api.deleteArticulo(id)
+        }catch (e :Exception) {
             throw e
         }
     }
